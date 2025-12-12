9916a0102ac6c4f52274137e651de78df0656b40
Pm Pranta <heyiampm@gmail.com>
2025-12-12 09:54:42 +0600

    Numpy File

---
+++ Numpy Array Properties/Num.Day7.py
@@ -0,0 +1,6 @@
+#with default values.future value replace
+# np.zero(shape) 3r 
+
+import numpy as np
+zeros_array = np.zeros(3)
+print(zeros_array)
---
+++ Numpy Array Properties/Num.Day8.py
@@ -0,0 +1,4 @@
+#one shape
+import numpy as np
+ones_array = np.ones((2,3))       # 2 is row.and 3 in columns.
+print(ones_array)
