ecff4609aaa096e466ac43def7e450a369a36d08
Pm Pranta <heyiampm@gmail.com>
2025-12-14 00:22:42 +0600

    Numpy File

---
+++ Numpy Array Properties/Num.Day14.py
@@ -0,0 +1,9 @@
+import numpy as np 
+#number of dimension. 
+arr_1 =np.array([10,20,30])
+arr_2 = np.array([[40,50,60],[70,80,90]])
+arr_3 = np.array([[[1,2], [3,4], [5,6], [7,8]]])
+
+print(arr_1.ndim)
+print(arr_2.ndim)
+print(arr_3.ndim)
