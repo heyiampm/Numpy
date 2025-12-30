bbb05c6a85fa78edd18049090029d454f0041ffc
Pm Pranta <heyiampm@gmail.com>
2025-12-30 10:54:51 +0600

    Numpy File

---
+++ Introduction/Num.Day1.py
@@ -0,0 +1,10 @@
+#Data science
+import numpy as np
+temperature = np.array ([32.5, 31.8, 33.0, 35.2, 36.6])
+average = np.mean(temperature)
+print(average)
+
+# total = 0
+# for tem in temperature:
+#           total += t
+# average = total/len(temperature)
